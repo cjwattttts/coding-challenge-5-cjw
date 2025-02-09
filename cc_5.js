764d84186f6a21fca8dbc80d7456867bc46a6c1d
cjwattttts <cameron.jwatts@outlook.com>
2025-02-09 18:54:10 -0500

    "Task 3 - Shopping Cart"

--- cc_5.js
+++ cc_5.js
@@ -4,6 +4,7 @@ let customer = { // establishes customer details
     age: 35,
     email: "john.doe@email.com"
 };
+
 console.log(`Customer Name: ${customer.name}`); // logs customer name 
 console.log(`Customer Age: ${customer.age}`); // logs customer age
 console.log(`Customer Email: ${customer.email}`); // logs customer email
@@ -14,8 +15,19 @@ let order = { // establishes order details
     totalAmouint: 150,
     status: "Processing:"
 displayOrder: function() {
+
     console.log(`Order ID: ${this.orderId}`); // logs order ID
     console.log(`Total Amount: ${this.totalAmount}`); // logs the total order amount
     console.log(`Order Status: ${this.status}`); // logs the order status
 }
 };
+
+//TASK 3 - Managing Shopping Cart
+let cartItems = ["Eggs, Milk, Chicken"]; // establishes initial cart
+cartItems.push("Orange Juice"); // adds orange juice
+cartItems.pop(); // removes orange juice
+cartItems.unshift("Kiwis") // adds kiwis to the beginning of the list
+cartItems.shift(); // removes kiwis
+
+console.log(cartItems); // logs finilized cart items 
+
