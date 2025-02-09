f234462233e976a2d0bd05bdb43280be49e7419e
cjwattttts <cameron.jwatts@outlook.com>
2025-02-09 18:45:24 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -7,3 +7,15 @@ let customer = { // establishes customer details
 console.log(`Customer Name: ${customer.name}`); // logs customer name 
 console.log(`Customer Age: ${customer.age}`); // logs customer age
 console.log(`Customer Email: ${customer.email}`); // logs customer email
+
+// TASK 2 - Creating Order Details
+let order = { // establishes order details
+    orderId: 12345,
+    totalAmouint: 150,
+    status: "Processing:"
+displayOrder: function() {
+    console.log(`Order ID: ${this.orderId}`); // logs order ID
+    console.log(`Total Amount: ${this.totalAmount}`); // logs the total order amount
+    console.log(`Order Status: ${this.status}`); // logs the order status
+}
+};
