62d243fa32d2375c8452ef8f294987065ee25920
cjwattttts <cameron.jwatts@outlook.com>
2025-02-09 18:37:38 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+// TASK 1 - Creating Customer Profile
+let customer = { // establishes customer details
+    name: "John Doe", 
+    age: 35,
+    email: "john.doe@email.com"
+};
+console.log(`Customer Name: ${customer.name}`); // logs customer name 
+console.log(`Customer Age: ${customer.age}`); // logs customer age
+console.log(`Customer Email: ${customer.email}`); // logs customer email
