90150904016934d3735a91620daaf0a99f4f3048
cjwattttts <cameron.jwatts@outlook.com>
2025-02-09 19:07:10 -0500

    Comment Cleanup

--- cc_5.js
+++ cc_5.js
@@ -5,9 +5,9 @@ let customer = { // establishes customer details
     email: "john.doe@email.com"
 };
 
-console.log(`Customer Name: ${customer.name}`); // logs customer name 
-console.log(`Customer Age: ${customer.age}`); // logs customer age
-console.log(`Customer Email: ${customer.email}`); // logs customer email
+console.log(`Customer Name: ${customer.name}`);  
+console.log(`Customer Age: ${customer.age}`); 
+console.log(`Customer Email: ${customer.email}`); 
 
 // TASK 2 - Creating Order Details
 let order = { // establishes order details
@@ -16,9 +16,9 @@ let order = { // establishes order details
     status: "Processing:"
 displayOrder: function() {
 
-    console.log(`Order ID: ${this.orderId}`); // logs order ID
-    console.log(`Total Amount: ${this.totalAmount}`); // logs the total order amount
-    console.log(`Order Status: ${this.status}`); // logs the order status
+    console.log(`Order ID: ${this.orderId}`); 
+    console.log(`Total Amount: ${this.totalAmount}`); 
+    console.log(`Order Status: ${this.status}`); 
 }
 };
 
@@ -29,22 +29,24 @@ cartItems.pop(); // removes orange juice
 cartItems.unshift("Kiwis") // adds kiwis to the beginning of the list
 cartItems.shift(); // removes kiwis
 
-console.log(cartItems); // logs finilized cart items 
+console.log(cartItems);  
 
 //TASK 4 - Adjusting prices
 let prices = [100, 200, 300]; // sets initial prices
 let discountedPrices = prices.map(price => price * 0.9); // applies at 10 percent discount 
 
-console.Log(discountedPrices); // logs the discounted prices
+console.Log(discountedPrices); 
 
 //TASK 5 - Filtering Products
 let inventory = [5, 0, 12, 8, 0]; // declares product quantities
 let availableStock = inventory.filter(quanitity => quanitity > 0); // removes products with zero stock
 
-console.log(availableStock); // logs filtered out inventory
+console.log(availableStock);
 
 //TASK 6 - Calculating Revenue
 let sales = [500, 300, 200, 400]; // sets sales amounts
 let totalRevenue = sales.reduce((sum, num) => sum + num, 0); // calulcates total revenue
 
 console.log(totalRevenue); 
+
+
